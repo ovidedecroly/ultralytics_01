33b876bb793ac08b23b42597256e844172c210b8
Ovide Academy <ovide@staff.uns.ac.id>
2024-08-28 12:36:54 +0700

    init

---
+++ ultralytics/nn/modules/convkan/__init__.py
@@ -0,0 +1,6 @@
+from .fastkan import FastKANLayer
+from .kanlinear import KANLinear
+from .convkan_layer import ConvKAN
+from .fastkanconv import FastConvKAN
+
+__all__ = ["KANLinear", "ConvKAN", "FastConvKAN"]
